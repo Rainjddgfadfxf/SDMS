04c9db227cde1e30b8358cd275aee65d90e2ed7f
Rainjddgfadfxf <1946124827@qq.com>
2020-11-18 17:39:46 +0800

    初步界面

---
+++ SDMS.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SDMS.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult StudentManage()
+        {
+            return View();
+        }
+        public ActionResult DormitoryManage()
+        {
+            return View();
+        }
+        public ActionResult StaffManage()
+        {
+            return View();
+        }
+    }
+}
---
+++ SDMS.WebUI/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SDMS.WebUI.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult RoomService()//寝室报修
+        {
+            return View();
+        }
+        public ActionResult RenInfo()//租赁查询
+        {
+            return View();
+        }
+        public ActionResult Utilities()//水电费
+        {
+            return View();
+        }
+    }
+  
+}
